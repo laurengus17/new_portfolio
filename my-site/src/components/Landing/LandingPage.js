7e628febd3c7a343e5e182243b5a7a4f72e66fb3
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-10-27 17:56:07 -0700

    nav

---
+++ my-site/src/components/Landing/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Landing.css'
+
+const LandingPage = () => {
+    return (
+        <div className='landing_page_container'>
+            <div className='selfie_container'>
+                <h2>Hi, Lauren here. Nice to meet you!</h2>
+                {/* <img src={me} alt='Lauren' className='portrait_image' /> */}
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
---
+++ my-site/src/components/NavBar/NavBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+        <ul className='navbar_ul'>
+            <li className='navbar_list'>
+                <Link to='/' exact={true} activeClassName='active' className='navbar_link'>
+                <i className='fas fa-landmark fa-2x' />
+                <span className='navbar_text'>Home</span>
+                </Link>
+            </li>
+            <li className='navbar_list'>
+                <Link to='/about' exact={true} activeClassName='active' className='navbar_link'> 
+                <i className='fas fa-door-open fa-2x' />
+                <span className='navbar_text'>About Me</span>
+                </Link>
+            </li>
+            <li className='navbar_list'>
+                <Link to='/projects' exact={true} activeClassName='active' className='navbar_link'> 
+                <i className='fas fa-fire-alt fa-2x' />
+                <span className='navbar_text'>Projects</span>
+                </Link>
+            </li>
+            <li className='navbar_list'>
+                <Link to='/resume' exact={true} activeClassName='active' className='navbar_link'> 
+                <i className='fas fa-file-code fa-2x' />
+                <span className='navbar_text'>Resume</span>
+                </Link>
+            </li>
+            <li className='navbar_list'>
+                <Link to='/contact' exact={true} activeClassName='active' className='navbar_link'> 
+                <i className='fas fa-paper-plane fa-2x' />
+                <span className='navbar_text'>Contact Me</span>
+                </Link>
+            </li>
+        </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
