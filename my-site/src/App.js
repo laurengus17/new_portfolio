b51431bfaf43116a2356a0fc7090195727beb264
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-10-27 18:06:11 -0700

    caught up

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import LandingPage from './components/LandingPage/LandingPage';
+import ResumeLanding from './components/Resume/ResumeLanding';
+import ProjectsLanding from './components/Projects/ProjectsLanding';
+import ContactLanding from './components/Contact/ContactLanding';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
+      <Footer />
+      <Switch>
+        <Route path='/' exact={true}>
+          <LandingPage/>
+        </Route>
+        <Route path='/resume' exact={true}>
+          <ResumeLanding />
+        </Route>
+        <Route path='/projects' exact={true}>
+          <ProjectsLanding />
+        </Route>
+        <Route path='/contact' exact={true}>
+          <ContactLanding />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
---
+++ my-site/src/components/Resume/ResumeLanding.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Resume.css';
+// download resume and put list of skills here
+
+const ResumeLanding = () => {
+    return (
+        <div className='resume_landing_container'>
+            <h2>Technologies - figure out how to show this</h2>
+            <div>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg" alt="aws" width="40" height="40"></img>
+                <img src="https://img.icons8.com/plasticine/452/bash.png" alt="bash" width="40" height="40"></img>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"></img>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width="40" height="40"></img>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT1PKsfJXnxOqnTRiIZ8VcdJDYBXD-qZnnpw&amp;usqp=CAU" alt="express" width="40" height="40"></img>
+                <img src="https://www.kindpng.com/picc/m/188-1882416_flask-python-logo-hd-png-download.png" alt="flask" width="40" height="40"></img>
+                <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"></img>
+                <img src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg" alt="heroku" width="40" height="40"></img>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"></img>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"></img>
+                <img src="https://www.vectorlogo.zone/logos/mochajs/mochajs-icon.svg" alt="mocha" width="40" height="40"></img>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"></img>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"></img>
+                <img src="https://cdn.worldvectorlogo.com/logos/pug.svg" alt="pug" width="40" height="40"></img>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg" alt="photoshop" width="40" height="40"></img>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40"></img>
+                <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="40" height="40"></img>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"></img>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"></img>
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" alt="redux" width="40" height="40"></img>
+            </div>
+        </div>
+    );
+}
+
+export default ResumeLanding;
