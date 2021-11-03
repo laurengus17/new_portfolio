cf5c94eb7821161d1f40824789ee87de2b65df38
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-11-03 10:48:02 -0700

    about me

--- my-site/src/components/About/AboutMeLanding.js
+++ my-site/src/components/About/AboutMeLanding.js
@@ -6,6 +6,10 @@ const AboutMeLanding = () => {
         <div className='about_page_container'>
             <div className='about_intro_container'>
                 <h2>ABOUT ME</h2>
+                <p>I am a curious person with a creative background who loves to tell stories. The reason I feel in love with programming 
+                    was discovering that there is never only one solution or one answer to any problem, to any question. Each programmer 
+                    tells their own story in their own way using the same shared languages. 
+                </p>
             </div>
         </div>
     );
