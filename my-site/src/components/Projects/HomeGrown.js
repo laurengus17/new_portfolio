7ad7b62f9c8ea5f0cebaa401866f6b4e642ae576
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-11-04 15:03:50 -0700

    images

--- my-site/src/components/Projects/HomeGrown.js
+++ my-site/src/components/Projects/HomeGrown.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import HG_browse from '../Images/HG_browse';
-import HG_user from '../Images/HG_user';
+import HG_browse from '../Images/HG_browse.png';
+import HG_user from '../Images/HG_user.png';
 import './Projects.css'
 
 const HomeGrown = () => {
     return (
         <div container='project_container'>
-            <h2>PROJECT HERE</h2>
-            
+            <img className='project_image' src={HG_browse} alt='HG browse'/>
+            <img className='project_image' src={HG_user} alt='HG_user' />
         </div>
     );
 }
