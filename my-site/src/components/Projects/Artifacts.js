11caac1826333712ac79afae567b426c36bcc6bc
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-11-04 15:34:04 -0700

    imports

--- my-site/src/components/Projects/Artifacts.js
+++ my-site/src/components/Projects/Artifacts.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import A_user from '../Images/A_user.png';
+import A_browse from '../Images/A_browse.png';
+import A_saber from '../Images/A_saber.png';
 import './Projects.css'
 
 const Artifacts = () => {
     return (
         <div container='project_container'>
-            <h2>PROJECT HERE</h2>
+            {/* <img className='project_image' src={HG_browse} alt='HG browse'/>
+            <img className='project_image' src={HG_user} alt='HG_user' /> */}
         </div>
     );
 }
--- my-site/src/components/Projects/KettleOverflow.js
+++ my-site/src/components/Projects/KettleOverflow.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import KO_question from '../Images/KO_question.png';
+import KO_search from '../Images/KO_search.png';
 import './Projects.css'
 
 const KettleOverflow = () => {
     return (
         <div container='project_container'>
-            <h2>PROJECT HERE</h2>
+            {/* <img className='project_image' src={HG_browse} alt='HG browse'/>
+            <img className='project_image' src={HG_user} alt='HG_user' /> */}
         </div>
     );
 }
