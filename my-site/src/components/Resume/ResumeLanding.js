849f42d39e2e0a7c3d7ba85c948c9abd397bd30b
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-11-04 16:41:40 -0700

    buttons

--- my-site/src/components/Resume/ResumeLanding.js
+++ my-site/src/components/Resume/ResumeLanding.js
@@ -1,34 +1,69 @@
 import React from 'react';
+import Flask from '../Images/Flask.png';
+import AWS from '../Images/AWS.png';
+import bash from '../Images/bash.png';
+import CSS from '../Images/css3.svg';
+import docker from '../Images/docker.svg';
+import express from '../Images/express.png';
+import git from '../Images/git.svg';
+import heroku from '../Images/heroku.svg';
+import html from '../Images/html.svg';
+import JS from '../Images/JS.svg';
+import mocha from '../Images/mocha.svg';
+import MYSQL from '../Images/MYSQL.svg';
+import node from '../Images/nodejs.svg';
+import pug from '../Images/pug.svg';
+import PS from '../Images/photoshop.svg';
+import postgres from '../Images/postgres.svg';
+import postman from '../Images/postman.svg';
+import python from '../Images/python.svg';
+import react from '../Images/react.svg';
+import redux from '../Images/redux.svg';
+import resume from '../ExternalFiles/Gustafson_Lauren_Resume.pdf';
 import './Resume.css';
 // download resume and put list of skills here
 
 const ResumeLanding = () => {
     return (
-        <div className='resume_landing_container'>
-            <h2>Technologies - figure out how to show this</h2>
+        <>
+        <div className='resume_download_container'>
+            <a href={resume} target='_blank'>
+                <button className='resume_button'>
+                    Resume Link
+                </button>
+            </a>
+            <a href={resume} download='Gustafson_Lauren_Resume'>
+                <button className='resume_button'>
+                    Download My Resume
+                </button>
+            </a>
+        </div>
+        <div className='resume_tech_container'>
+            <p className='tech_title'>Technologies</p>
             <div>
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg" alt="aws" width="40" height="40"></img>
-                <img src="https://img.icons8.com/plasticine/452/bash.png" alt="bash" width="40" height="40"></img>
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"></img>
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width="40" height="40"></img>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT1PKsfJXnxOqnTRiIZ8VcdJDYBXD-qZnnpw&amp;usqp=CAU" alt="express" width="40" height="40"></img>
-                <img src="https://www.kindpng.com/picc/m/188-1882416_flask-python-logo-hd-png-download.png" alt="flask" width="40" height="40"></img>
-                <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"></img>
-                <img src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg" alt="heroku" width="40" height="40"></img>
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"></img>
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"></img>
-                <img src="https://www.vectorlogo.zone/logos/mochajs/mochajs-icon.svg" alt="mocha" width="40" height="40"></img>
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"></img>
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"></img>
-                <img src="https://cdn.worldvectorlogo.com/logos/pug.svg" alt="pug" width="40" height="40"></img>
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg" alt="photoshop" width="40" height="40"></img>
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40"></img>
-                <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="40" height="40"></img>
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"></img>
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"></img>
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" alt="redux" width="40" height="40"></img>
+                <img src={AWS} alt="aws" width="100" height="100"></img>
+                <img src={bash} alt="bash" width="100" height="100"></img>
+                <img src={CSS} alt="css3" width="100" height="100"></img>
+                <img src={docker} alt="docker" width="100" height="100"></img>
+                <img src={express} alt="express" width="100" height="100"></img>
+                <img src={Flask} alt="flask" width="100" height="100"></img>
+                <img src={git} alt="git" width="100" height="100"></img>
+                <img src={heroku} alt="heroku" width="100" height="100"></img>
+                <img src={html} alt="html5" width="100" height="100"></img>
+                <img src={JS} alt="javascript" width="100" height="100"></img>
+                <img src={mocha} alt="mocha" width="100" height="100"></img>
+                <img src={MYSQL} alt="mysql" width="100" height="100"></img>
+                <img src={node} alt="nodejs" width="100" height="100"></img>
+                <img src={pug} alt="pug" width="100" height="100"></img>
+                <img src={PS} alt="photoshop" width="100" height="100"></img>
+                <img src={postgres} alt="postgresql" width="100" height="100"></img>
+                <img src={postman} alt="postman" width="100" height="100"></img>
+                <img src={python} alt="python" width="100" height="100"></img>
+                <img src={react} alt="react" width="100" height="100"></img>
+                <img src={redux} alt="redux" width="100" height="100"></img>
             </div>
         </div>
+        </>
     );
 }
 
