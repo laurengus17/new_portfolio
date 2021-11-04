70384788f662769384cb2575cba2177217ddb4bc
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-11-04 16:01:00 -0700

    contact first draft

--- my-site/src/components/Contact/ContactLanding.js
+++ my-site/src/components/Contact/ContactLanding.js
@@ -4,30 +4,30 @@ import './Contact.css';
 const ContactLanding = () => {
     return (
         <div className='contact_landing_container'>
-            <p className='contact_title'>Get In Touch</p>
+            <p className='contact_title'>Get In Touch 👋</p>
             <ul>
                 <li className='contact_list'>
                     <a href='https://github.com/laurengus17'>
-                    <i className='fab fa-github fa-3x'/>
-                    <span className='portal_name'>GitHub</span>
+                        <i className='fab fa-github fa-4x'/>
+                        <span className='portal_name'>GitHub</span>
                     </a>
                 </li>
                 <li className='contact_list'>
                     <a href='https://www.linkedin.com/in/lauren-gustafson-7b8877b3/'>
-                    <i className='fab fa-linkedin fa-3x' />
-                    <span className='portal_name'>LinkedIn</span>
+                        <i className='fab fa-linkedin fa-4x' />
+                        <span className='portal_name'>LinkedIn</span>
                     </a>
                 </li>
                 <li className='contact_list'>
                     <a href='https://angel.co/u/lauren-gustafson-4'>
-                    <i className='fab fa-angellist fa-3x' />
-                    <span className='portal_name'>AngelList</span>
+                        <i className='fab fa-angellist fa-4x' />
+                        <span className='portal_name'>AngelList</span>
                     </a>
                 </li>
                 <li className='contact_list'>
                     <a href='mailto:lauren.gustafson17@gmail.com'>
-                    <i className='fas fa-envelope-square fa-3x'/>
-                    <span className='portal_name'>Email</span>
+                        <i className='fas fa-envelope-square fa-4x'/>
+                        <span className='portal_name'>Email</span>
                     </a>
                 </li>
             </ul>
